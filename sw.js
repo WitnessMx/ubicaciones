d09e23e47e9dd9c80f31eb99ecce4eac90dbd3f0
WitnessMx <71900846+WitnessMx@users.noreply.github.com>
2023-10-17 01:03:01 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,14 +6,12 @@ const assets = [
   "/ubicaciones/src/scripts/leaflet-search.js",
   "/ubicaciones/src/scripts/mapa.js",
   "/ubicaciones/src/UBICACIONES.json",
-  "/ubicaciones/src/scripts/leaflet.js",
   "/ubicaciones/src/images/Layout_010221_01.png",
   "/ubicaciones/src/images/loader.gif",
   "/ubicaciones/src/images/search-icon.png",
   "/ubicaciones/src/images/search-icon-mobile.png",
   "/ubicaciones/src/images/marcadores/marcador2.png",
   "/ubicaciones/src/images/ubi.png",
-  "/ubicaciones/src/styles/leaflet.css",
   "/ubicaciones/src/styles/leaflet-search.css",
   "/ubicaciones/src/styles/style_calibracion2023.css"
 
